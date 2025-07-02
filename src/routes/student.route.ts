df57f2825311a3e45f5dd27823b6c7d852def065
Aman <rajaman9355@gmail.com>
2025-07-02 10:03:24 +0530

    Changed Leaderboard Structure

--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -8,6 +8,7 @@ import {
   editStudent,
   deleteStudent,
   addStudent,
+  getScoreGraph,
 } from "../controllers/student.controller.js";
 import { authStudent, authAdmin } from "../middlewares/auth.middleware.js";
 import { uploadExcel } from "../middlewares/uploadExcel.js";
@@ -17,6 +18,7 @@ const studentRouter = Router();
 studentRouter.post("/add-student",authAdmin, uploadExcel.single("file"), addStudent);
 studentRouter.get("/get-student-profile", authStudent, getStudentDetails);
 studentRouter.get("/get-student-academics", authStudent, getStudentAcademicDetails);
+studentRouter.get('/get-course-graph',authStudent,getScoreGraph)
 studentRouter.get("/get-batch-leaderboard", authStudent, getStudentMarksByBatch);
 studentRouter.get("/get-department-leaderboard", authStudent, getStudentMarksByDepartment);
 
